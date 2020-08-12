4d0e93db87f87296ea7421dd51d576f8e25a326a
sharathrushi <sharathrushi@gmail.com>
2020-08-12 17:10:55 +0530

    Update evaluate_fraud.py

--- src/app/api/evaluate_fraud.py
+++ src/app/api/evaluate_fraud.py
@@ -12,9 +12,31 @@ router = APIRouter()
 )
 async def evaluate_fraud(request: Request):
     print(request.headers)
+    headers = request.headers
     body_xml = await request.body()
     root = ET.fromstring(body_xml)
-    print(root.attrib)
-    print(request.url.port)
-    response_object = {"id": "12345", "url": "google.com"}
-    return response_object
+
+    print('########### START of Reading Request ##########')
+    email_address = root[0][1][7][4][1]
+    print(email_address.text)
+    CreDtTm = root[0][0][1]
+    print('creDTM ', CreDtTm.text)
+    print('######### END of Reading Request###########')
+
+    print('START of Calling fraud detector')
+    
+    print('End of calling Fraud dector')
+
+    print('START Reading Response and Apending fraud results')
+    response = ET.parse('response.xml').getroot()
+    print(response[2][1])
+    elem = response[2][1]
+    elem[0].text = 'text'
+    elem[1].text = 'RULE_NAME'
+    elem[2].text = 'RESULT'
+    elem[3].text = 'CATEGORY'
+    response[2].append(elem)
+    print('######### END ###########')
+
+    response_string = ET.tostring(response)
+    return Response(content=response_string, media_type="application/xml")
